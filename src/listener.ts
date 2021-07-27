ac9810ddb0b26223d053250bbf51b41fe3139941
Charles Allison <soprinye@gmail.com>
2021-07-27 17:20:10 +0100

    Gracefully shutdown client, notify server.

--- src/listener.ts
+++ src/listener.ts
@@ -9,6 +9,11 @@ const stan = nats.connect('test-cluster', randomBytes(4).toString('hex'), {
 stan.on('connect', () => {
     console.log('Listener connected to nats');
 
+    stan.on('close', () => {
+        console.log('NATS connection closed.');
+        process.exit();
+    });
+
     const options = stan.subscriptionOptions()
     .setManualAckMode(true); // used to indicate that the serve should continue sending the event to the subscribers until the msg.ack() method is called.
 
@@ -25,3 +30,7 @@ stan.on('connect', () => {
         msg.ack();
     })
 });
+
+
+process.on('SIGNINT', () => stan.close());
+process.on('SIGTERM', () => stan.close());
